dd1a1e2a46e90f4253c6b35e6daa5bd200eec8a8
dorjoo <dorjoohover@gmail.com>
2025-01-06 19:48:55 +0800

    a

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -14,7 +14,7 @@ import { DataSource } from 'typeorm';
             host: 'localhost',
             port: 5432,
             // username: 'postgres',
-            username: 'hire',
+            username: 'dorjoo',
             password: 'root',
             database: 'hire',
             entities: [__dirname + '/../**/*.entity{.ts,.js}'],
