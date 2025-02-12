fee8beb4ac80bf275d1cfeaf55ea2d963bc5c5f7
dorjoo <dorjoohover@gmail.com>
2025-02-12 17:05:43 +0800

    a

--- src/app/feedback/entities/feedback.entity.ts
+++ src/app/feedback/entities/feedback.entity.ts
@@ -8,7 +8,7 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
-@Entity('Feedback')
+@Entity('feedback')
 export class FeedbackEntity {
   @PrimaryGeneratedColumn('increment')
   id?: number;
