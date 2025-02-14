c7763a2a433d2ca8d5d92a10c9825c1027b9dd8a
dorjoo <dorjoohover@gmail.com>
2025-02-14 15:09:52 +0800

    'a'

--- src/auth/guards/role/role.guard.ts
+++ src/auth/guards/role/role.guard.ts
@@ -24,7 +24,6 @@ export class RolesGuard extends AuthGuard('jwt') {
       const { user } = context.switchToHttp().getRequest();
 
       const userRole = user.role;
-      console.log(userRole);
       if (
         user.app === 'app' &&
         (userRole === Role.admin ||
