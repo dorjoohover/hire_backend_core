1c5296c2b07a9aa87248c9526b8026bcc6d602fc
dorjoo <dorjoohover@gmail.com>
2025-02-20 13:48:54 +0800

    a

--- src/app/blog/blog.controller.ts
+++ src/app/blog/blog.controller.ts
@@ -11,17 +11,21 @@ import {
 import { BlogService } from './blog.service';
 import { CreateBlogDto } from './dto/create-blog.dto';
 import { UpdateBlogDto } from './dto/update-blog.dto';
-import { ApiParam } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiParam } from '@nestjs/swagger';
+import { Public } from 'src/auth/guards/jwt/jwt-auth-guard';
+import { Roles } from 'src/auth/guards/role/role.decorator';
+import { Role } from 'src/auth/guards/role/role.enum';
 
 @Controller('blog')
+@ApiBearerAuth('access-token')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
-
+  @Roles(Role.admin, Role.super_admin, Role.tester)
   @Post()
   create(@Body() dto: CreateBlogDto, @Request() { user }) {
     return this.blogService.create(dto, user['id']);
   }
-
+  @Public()
   @Get('all/:type/:limit/:page')
   @ApiParam({ name: 'limit' })
   @ApiParam({ name: 'type' })
@@ -33,17 +37,19 @@ export class BlogController {
   ) {
     return this.blogService.findAll(+type, limit, page);
   }
-
+  @Public()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.blogService.findOne(+id);
   }
 
+  @Roles(Role.admin, Role.super_admin, Role.tester)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateBlogDto: CreateBlogDto) {
     return this.blogService.update(+id, updateBlogDto);
   }
 
+  @Roles(Role.admin, Role.super_admin, Role.tester)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.blogService.remove(+id);
