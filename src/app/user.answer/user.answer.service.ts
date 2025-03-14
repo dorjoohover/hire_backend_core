fed325570af39faa2715409c56844757d18da951
dorjoo <dorjoohover@gmail.com>
2025-03-14 12:35:22 +0800

    'a'

--- src/app/user.answer/user.answer.service.ts
+++ src/app/user.answer/user.answer.service.ts
@@ -120,17 +120,18 @@ export class UserAnswerService extends BaseService {
           user,
         );
 
-        const send = await this.mailService.sendMail({
-          to: user.email,
-          subject: 'Тайлан хүлээн авах',
-          html: `<div>
+        if (!response.visible)
+          await this.mailService.sendMail({
+            to: user.email,
+            subject: 'Тайлан хүлээн авах',
+            html: `<div>
           <p>Та тайлангаа <a href=https://srv666826.hstgr.cloud/api/v1/exam/pdf/${dto.data[0].code}>эндээс</a> татаж авна уу.</p>
           <p>Тайлан pdf хэлбэрээр татагдах болно.</p>
            <p>Асууж, тодруулах зүйл байвал <a href=mailto:info@hire.mn>info@hire.mn</a> хаягаар, <a href=tel:976-9909 9371>976-9909 9371</a> дугаараар холбогдоорой. </p>
            <p>Манайхаар үйлчлүүлж байгаад тань баярлалаа.</p>
            <p>Шуудангийн хаяг: Улаанбаатар хот, Баянзүрх дүүрэг, 1-р хороо Энхтайвны өргөн чөлөө-5, СЭЗИС, Б байр, 7-р давхар, 13381, Ш/Н: Улаанбаатар-49</p>
            </div>`,
-        });
+          });
         return response.visible ? { visible: response.visible } : response;
       }
 
