b23db35492d50a60287322487d43d678351557f2
dorjoo <dorjoohover@gmail.com>
2025-03-24 13:26:06 +0800

    'a'

--- src/app/user.service/user.service.controller.ts
+++ src/app/user.service/user.service.controller.ts
@@ -83,7 +83,7 @@ export class UserServiceController {
   @Get('user/:id')
   @ApiParam({ name: 'id' })
   findById(@Param('id') id: string, @Request() { user }) {
-    return this.userServiceService.findByUser(+id, +user['id'], user['email']);
+    return this.userServiceService.findByUser(+id, +user['id']);
   }
 
   @Get('find/:id')
--- src/app/user.service/user.service.service.ts
+++ src/app/user.service/user.service.service.ts
@@ -111,44 +111,55 @@ export class UserServiceService extends BaseService {
 
   // public async
 
-  public async findByUser(assId: number, id: number, email: string) {
-    // const response = await this.dao.findByUser(assId, id);
-    // const exams = await this.examDao.findByUser([], id);
-    // const responses = [
-    //   ...response,
-    //   ...Array.from({ length: exams.length }, (_, i) => i + 1).map((_) => {
-    //     return { exams };
-    //   }),
-    // ].reduce((acc, obj) => {
-    //   acc.exams.
-    // })
-    // const res = [];
-    // for (const response of responses) {
-    //   const exams = response.exams;
-    //   const examResults = [];
-    //   for (const exam of exams) {
-    //     const result = await this.result.findOne(exam.code);
-    //     examResults.push({
-    //       ...exam,
-    //       result: result,
-    //     });
-    //   }
-    //   res.push({ ...response, exams: examResults });
-    // }
-
-    // return res;
-    const exams = await this.examDao.findAll(assId, email);
+  public async findByUser(assId: number, id: number) {
+    const responses = await this.dao.findByUser(assId, id, 0);
     const res = [];
-    for (const exam of exams) {
-      const result = await this.result.findOne(exam.code);
-      const service = await this.dao.findByUser(assId, id, exam.service.id);
-      res.push({
-        ...exam,
-        result: result,
-        invited: service.length == 0,
-      });
+    const ex = [];
+    const data = await this.examDao.findByUser([], id);
+    for (const response of responses) {
+      const exams = response.exams;
+      const examResults = [];
+      for (const exam of exams) {
+        const result = await this.result.findOne(exam.code);
+        examResults.push({
+          ...exam,
+          result: result,
+        });
+      }
+      ex.push(exams);
+      res.push({ ...response, exams: examResults });
     }
-    return res;
+
+    const filtered = data.filter(
+      (obj1) => !ex.some((obj2) => obj2.code === obj1.code),
+    );
+
+    const invited = await Promise.all(
+      filtered.map(async (f) => {
+        const result = await this.result.findOne(f.code);
+        return {
+          ...f,
+          result,
+        };
+      }),
+    );
+
+    return {
+      data: res,
+      invited,
+    };
+    // const exams = await this.examDao.findAll(assId, email);
+    // const res = [];
+    // for (const exam of exams) {
+    //   const result = await this.result.findOne(exam.code);
+    //   const service = await this.dao.findByUser(assId, id, exam.service.id);
+    //   res.push({
+    //     ...exam,
+    //     result: result,
+    //     invited: service.length == 0,
+    //   });
+    // }
+    // return res;
   }
 
   public async createExam(dto: CreateExamServiceDto, id: number, role: number) {
