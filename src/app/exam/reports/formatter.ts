4026407d4cf7ad546f1a458899e30a5a86cb9724
dorjoo <dorjoohover@gmail.com>
2025-04-09 11:26:52 +0800

    a

--- src/app/exam/reports/formatter.ts
+++ src/app/exam/reports/formatter.ts
@@ -51,7 +51,7 @@ export function generateQRCodeSync(url: string): Buffer {
 
     QRCode.toCanvas(canvas, url, {
       errorCorrectionLevel: 'Q',
-      version: 2,
+      version: 3,
       margin: 1,
       color: {
         dark: '#FFFFFF',
