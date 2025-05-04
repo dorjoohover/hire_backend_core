9c800c4bf615de00937886f9e6f3c1a0f8d28c6b
dorjoo <dorjoohover@gmail.com>
2025-05-05 04:41:54 +0800

    a

--- src/app/user.answer/user.answer.service.ts
+++ src/app/user.answer/user.answer.service.ts
@@ -86,7 +86,9 @@ export class UserAnswerService extends BaseService {
             let answerCategory = answer.matrix
               ? await this.questionAnswerMatrixDao.findOne(answer.matrix)
               : await this.questionAnswerDao.findOne(answer.answer);
-
+            console.log(answer.matrix === undefined);
+            console.log((answerCategory as QuestionAnswerEntity).reverse);
+            console.log(answerCategory.question.maxValue - answer.point + 1);
             const point =
               answer.matrix === undefined &&
               (answerCategory as QuestionAnswerEntity).reverse === true
