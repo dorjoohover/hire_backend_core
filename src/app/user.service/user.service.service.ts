386d914e2529c2bb96506d0b4e91477ae279948f
dorjoo <dorjoohover@gmail.com>
2025-05-13 01:39:36 +0800

    a

--- src/app/user.service/user.service.service.ts
+++ src/app/user.service/user.service.service.ts
@@ -259,6 +259,7 @@ export class UserServiceService extends BaseService {
           if(+minute < 10) minute = `0${minute}`
           let second = `${date.getSeconds()}`
           if(+second < 10) second = `0${second}`
+          console.log(exam.service)
           await this.mailer.sendMail({
             to: email.email,
             subject: 'Танд тестийн урилга ирлээ',
@@ -326,7 +327,7 @@ export class UserServiceService extends BaseService {
                           </tr>
                           <tr>
                           <td style="font-family: 'Montserrat', sans-serif; font-size: 14px; line-height: 1.6; color: #333333; text-align: justify;">
-                                <br/>Эрхэм <strong>${email.lastname} ${email.firstname}</strong> танд <strong>${exam.service.user?.organizationName ?? ''}</strong> байгууллагаас <strong style="color: #ff5000;">${exam.service.assessment.name}</strong> онлайн тест, үнэлгээнд оролцох урилга илгээсэн байна. Та <a style="color: #ff5000; text-decoration: none;" href=https://hire.mn/exam/${email.code}>линк дээр дарж</a> тест, үнэлгээндээ оролцоно уу.
+                                <br/>Эрхэм <strong>${email.lastname} ${email.firstname}</strong> танд <strong>${exam.service.user?.organizationName ?? ''}</strong> байгууллагаас <strong style="color: #ff5000;">${exam?.service?.assessment?.name}</strong> онлайн тест, үнэлгээнд оролцох урилга илгээсэн байна. Та <a style="color: #ff5000; text-decoration: none;" href=https://hire.mn/exam/${email.code}>линк дээр дарж</a> тест, үнэлгээндээ оролцоно уу.
                             </td>
                           </tr>
                            <tr>
