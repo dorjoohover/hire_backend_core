66c6c45d05b0e236621a0bb4215c5e3b62e09864
dorjoo <dorjoohover@gmail.com>
2026-02-04 02:46:56 +0800

    port

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,7 +42,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       useFactory: () => ({
         type: 'postgres',
         host: process.env.DB_HOST,
-        port: Number(process.env.DB_HOST),
+        port: Number(process.env.DB_PORT),
         username: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
